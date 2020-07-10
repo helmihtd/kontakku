2507fcd7561ca58edbe557605e4f78d36707d873
helmihartadi <59297495+helmihartadi@users.noreply.github.com>
2020-07-10 09:29:24 +0700

    Updates

---
+++ precache-manifest.4a10f84f7c9968ceb6f7bb4ba318b866.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b80ab034aadfe9239ca7b7707e74cc1",
+    "url": "/kontakku/index.html"
+  },
+  {
+    "revision": "4cc0c72ec1e577020585",
+    "url": "/kontakku/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9c2cf252cc9b12c2781b",
+    "url": "/kontakku/static/css/main.3d8a04b3.chunk.css"
+  },
+  {
+    "revision": "4cc0c72ec1e577020585",
+    "url": "/kontakku/static/js/2.7df2a46f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kontakku/static/js/2.7df2a46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2cf252cc9b12c2781b",
+    "url": "/kontakku/static/js/main.545a4226.chunk.js"
+  },
+  {
+    "revision": "2393150c53bf10070a56",
+    "url": "/kontakku/static/js/runtime-main.7f78ae54.js"
+  },
+  {
+    "revision": "dc1099d08ba9da773f1c806c5c6b1c02",
+    "url": "/kontakku/static/media/PEPSI_pl.dc1099d0.woff"
+  }
+]);
